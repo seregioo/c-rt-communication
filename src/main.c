99b5dae28e3dd6c3809c0390a23e3b415bd32cc1
sergio <sergiohg818@gmail.com>
2026-04-23 18:34:30 +0200

    initial commit

---
+++ src/main.c
@@ -0,0 +1,11 @@
+/**
+ * @file main.c
+ * @author Sergio Hidalgo (sergiohg.dev@gmail.com)
+ * @brief Main file for the RealTime communication program
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * @copyright Copyright (c) Sergio Hidalgo 2026
+ */
+#include <stdio.h>
+
+int main() { printf("Initial struct\n"); }
