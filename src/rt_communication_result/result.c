150aa9413c3a745c3d7e43e4c8c22f01810a6722
sergio <sergiohg818@gmail.com>
2026-04-24 01:13:55 +0200

    rt_communication_result: RT communication result logic implementation

--- src/rt_communication_result/result.c
+++ src/rt_communication_result/result.c
@@ -8,6 +8,7 @@
  * @copyright Copyright (c) Sergio Hidalgo 2026
  */
 #include "rt_communication_result/result.h"
+#include <stdlib.h>
 
 struct rt_communication_result_e {
   rt_communication_result_type_t result_type;
@@ -22,27 +23,42 @@ rt_communication_result_t *rt_communication_result_create(
     void *result_value,
     rt_communication_result_callback_string_function_t
         rt_result_callback_funct) {
-  // TODO: implement this function
+  rt_communication_result_t *rt_result =
+      (rt_communication_result_t *)malloc(sizeof(rt_communication_result_t));
+
+  if (!rt_result) {
+    return NULL;
+  }
+
+  rt_result->result_type = result_type;
+  rt_result->sub_type = sub_type;
+  rt_result->result_value = result_value;
+  rt_result->rt_communication_result_callback_string_function =
+      rt_result_callback_funct;
+
+  return rt_result;
 }
 
 void rt_communication_result_destroy(rt_communication_result_t *rt_result) {
-  // TODO: implement this function
+  rt_result->result_value = NULL;
+  rt_result->rt_communication_result_callback_string_function = NULL;
+  free(rt_result);
 }
 
 rt_communication_result_type_t
 rt_communication_result_get_type(rt_communication_result_t *rt_result) {
-  // TODO: implement this function
+  return rt_result->result_type;
 }
 
 int rt_communication_result_get_subtype(rt_communication_result_t *rt_result) {
-  // TODO: implement this function
+  return rt_result->sub_type;
 }
 
 void *rt_communication_result_get_value(rt_communication_result_t *rt_result) {
-  // TODO: implement this function
+  return rt_result->result_value;
 }
 
 const char *
 rt_communication_result_get_string(rt_communication_result_t *rt_result) {
-  // TODO: implement this function
+  return rt_result->rt_communication_result_callback_string_function(rt_result);
 }
